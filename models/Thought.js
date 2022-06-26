53377fb02e93726c2f26a2dcefa8db553b6df7b3
Floki <ypr_tylerkeuler@aol.com>
2022-06-25 20:49:38 -0400

    logic for creating thoughts

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minItems: 0,
+            maxItems: 280,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    },
+);
+
+module.exports = reactionSchema;
