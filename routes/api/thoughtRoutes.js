00b6390c3e188c301e003120abfbf407a4156dfd
Floki <ypr_tylerkeuler@aol.com>
2022-06-25 21:21:12 -0400

    add logic for routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+  } = require('../../controllers/thoughtController.js');
+
+  router.route('/').get(getThoughts).post(createThought);
+
+  router.route('/:thoughtId/reactions').post(addReaction);
+
+  router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+  router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+  module.exports = router;
---
+++ routes/api/userroutes.js
@@ -0,0 +1,27 @@
+
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController.js');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend);
+
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
